10adfe05d04c744a76f521262c481fb3639a6568
Manuel <manuel.r.a@hotmail.com>
2019-10-20 21:33:46 -0600

    get user added

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,18 +1,49 @@
-import { Router, Request, Response, response } from "express";
-import User,{ IUser } from '../models/user';
+import { Router, Request, Response } from "express";
+import User, { IUser } from "../models/user";
+import { NativeError } from "mongoose";
 
 
-const router = Router();
 
-router.post('/user',(req:Request, res:Response)=>{
-    
+const userRoutes = Router();
+
+userRoutes.post('/user', (req: Request, res: Response) => {
+    let user: IUser = new User();
+    user.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.active = true;
+    user.save((err, user) => {
+        if (err) {
+            res.json({
+                ok: false,
+                err
+            });
+            return;
+        }
+        res.json({
+            ok: true,
+            data: { user }
+        });
+    })
+
 });
 
-router.get('/user',(req:Request, res:Response)=>{
-    res.json({
-        ok:true,
-        msg: "Everything is ok! "
-    });
+userRoutes.get('/user', (req: Request, res: Response) => {
+    User.find().exec((err: NativeError, results: IUser[]) => {
+        if (err) {
+            res.json({
+                ok: true,
+                err
+            });
+            return;
+        }
+        res.json({
+            ok: true,
+            data: {
+                users: results
+            }
+        })
+    })
 });
 
-export default router;
+export default userRoutes;
