c5de293953efa7796bd0c8cbab231537b67337c1
Manuel Ruelas <manuel.r.a@hotmail.com>
2019-10-22 18:51:19 -0600

    Blood request model added

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,14 @@ import user from "../models/user";
 import userRoutes from "./user";
 import bloodTypeRoutes from "./bloodType";
 import authRoutes from "./authentication";
+import bloodRequestRoutes from "./bloodRequest";
 
 let router = Router();
 
 router.use(userRoutes);
 router.use(bloodTypeRoutes);
 router.use(authRoutes);
+router.use(bloodRequestRoutes);
+
 
 export default router;
